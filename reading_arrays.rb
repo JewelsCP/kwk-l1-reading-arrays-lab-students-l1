5ffa183c31c8e2a7f2fe950c11842db076c58bb2
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-05 22:31:04 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -20,13 +20,13 @@ STUDENT_NAMES [5]
 end
 
 def first_student_by_method
-  STUDENT_NAMES [-1]
+  STUDENT_NAMES.first
 end
 
-# def last_student_by_method
-#   # Write a solution that returns the first student using the built in .last method
-# end
+def last_student_by_method
+  # Write a solution that returns the first student using the built in .last method
+end
 
-# # def first_second_and_third_students
-# #   # Write a solution that returns the first, second and third students
-# # end
+# def first_second_and_third_students
+#   # Write a solution that returns the first, second and third students
+# end
